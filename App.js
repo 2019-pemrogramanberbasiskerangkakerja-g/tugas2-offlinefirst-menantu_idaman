bfa4f85aa3a2304b51d0bf437bddfe29b832d3b6
Ivan Fadhila <ivan.fadhila70@gmail.com>
2019-03-04 20:57:11 +0700

    fix Login

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const users = require('./server/models').User;
+const jwtLogin = require('jwt-login');
 
 const app = express();
 const server = require('http').createServer(app);
@@ -18,6 +20,34 @@ require('./server/login/routes')(app);
 
 app.get('/', function(request, response) {
 	response.sendFile(path.join(__dirname + '/web/login/login.html'));
+});
+
+app.post("/kirim", function(req, res ){
+	console.log('request');
+	// res.end();
+
+	var data = req.body;
+	var user = data.username;
+	var password = data.password;
+
+	users
+		.findOne({
+			where: {username: user}
+		})
+		.then(async result => {
+			if (!result) {
+				return res.status(400).send('User not found'); 
+			} else if(result) {
+				if(password == result.password)
+				{
+					// jwtLogin.sign(req, res, user, 'topsecret', 1, false); 
+					return res.status(400).send('Auth success. OK password.');
+				}
+				else {
+					return res.status(400).send('Auth failed. Wrong password.'); 
+				}
+			}
+		});
 });
 
 const port = 1212;
