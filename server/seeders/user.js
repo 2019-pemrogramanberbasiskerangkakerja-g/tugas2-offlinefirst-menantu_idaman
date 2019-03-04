a19b6cfc0d91138449f192b3e1794291c42c1407
Muhammad Adib Arinanda <arinanda.adib@gmail.com>
2019-03-04 10:57:10 +0800

    update user seeder

--- server/seeders/user.js
+++ server/seeders/user.js
@@ -6,19 +6,19 @@ module.exports = {
             .create({
                 username: 'arinanda',
                 email: 'arinanda.adib@gmail.com',
-                password: 'Popokbayi1.'
+                password: '$2y$12$.Oe5SKvZ1KpxCOI3G2YsMeLNO7pi6Of1KCk3Vdyo.iHS8Z.CYW4Pu.'
             }),
         user
             .create({
                 username: 'ivan',
                 email: 'ivanfadhb@gmail.com',
-                password: 'Popokbayi1.'
+                password: '$2y$12$.Oe5SKvZ1KpxCOI3G2YsMeLNO7pi6Of1KCk3Vdyo.iHS8Z.CYW4Pu.'
             }),
         user
             .create({
                 username: 'sidqi',
                 email: 'sidqi@gmail.com',
-                password: 'Popokbayi1.'
+                password: '$2y$12$.Oe5SKvZ1KpxCOI3G2YsMeLNO7pi6Of1KCk3Vdyo.iHS8Z.CYW4Pu.'
             })
     }
 };
