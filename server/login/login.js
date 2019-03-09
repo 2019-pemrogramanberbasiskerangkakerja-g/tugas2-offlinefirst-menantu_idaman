c719dbed95af3e2867dd5baaf6e79638c8d73686
Muhammad Adib Arinanda <arinanda.adib@gmail.com>
2019-03-09 14:33:55 +0700

    fixing login method

--- server/login/login.js
+++ server/login/login.js
@@ -13,16 +13,14 @@ module.exports = {
                 where: {username: user}
             })
             .then(async result => {
+                const matches = await bcrypt.compare(password, result.password);
                 if (!result) {
                     return res.status(400).send('User not found'); 
-                } else if(result) {
-                    if(password == result.password)
-                    {
-                        return res.status(200).send('Logged in'); 
-                    }
-                    else {
-                        return res.status(400).send('Auth failed. Wrong password.'); 
-                    }
+                } else if(matches) {
+                    return res.status(200).send('Logged in'); 
+                }
+                else {
+                    return res.status(400).send('Auth failed. Wrong password.'); 
                 }
             });
     },
